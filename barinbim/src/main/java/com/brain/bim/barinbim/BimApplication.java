c41b5126dd2ee9fbaac285c359a3dbf1b1922600
hamid <hamid@DESKTOP-H92RLMG.fritz.box>
2018-06-04 20:04:32 +0200

    first commit

---
+++ barinbim/src/main/java/com/brain/bim/barinbim/BimApplication.java
@@ -0,0 +1,13 @@
+package com.brain.bim.barinbim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimApplication {
+  
+  public static void main(final String[] args) {
+    SpringApplication.run(BimApplication.class, args);
+  }
+  
+}
